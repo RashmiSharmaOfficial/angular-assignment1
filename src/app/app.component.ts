548c3ad42a268b0053316a9afac06c271ffd6712
rashmisharmaofficial <rashmisharmaofficial19019@gmail.com>
2021-12-28 13:10:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment1';
+  studName = 'Alexa'
+  studID = 101
+  studObj = { uid: 'alex', pwd: 'password' }
+
+  policyObj = {
+    no: 101,
+    holder: 'Rashmi',
+    nominee: 'Sister',
+    amtMaturity: 10000,
+    monthEmi: 20,
+    duration: 6
+  }
+
+  image = './assets/image/fruit.jpeg'
+
+  disableBtn = false;
+  enBtn() {
+    this.disableBtn = false;
+  }
+  disBtn() {
+    this.disableBtn = true;
+  }
+
+  verification(email: string) {
+    alert(`We have sent you code on ${email}`)
+  }
+
+  phoneValid(phone: string) {
+    if (phone.length < 10) {
+      alert('Phone number length is less then 10!!')
+    }
+    else {
+      alert('Valid phone number!!')
+    }
+  }
+
+  // two way data binding
+  varTwoWayDataBinding = 'rashmi Sharma'
+
+  verify(username: string, password: string) {
+    if (username == 'Rashmi' && password == 'password') {
+      alert('Valid user!')
+    }
+    else {
+      alert('Invalid user!')
+    }
+  }
+
+  //parent to child communication
+  employeeID = 's101'
+
+  //sending object
+  bigBasket = {
+    fruitName: 'Orange',
+    quantity: 12
+  }
+
 }
